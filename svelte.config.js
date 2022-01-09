b578f1c07b74bada59a5c71488bf72755e5ed467
jb-thery <jb.thery@flameapp.fr>
2022-01-09 22:45:44 +0100

    feat(Deploy): implement github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,11 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
     target: '#svelte'
   }
 };
